b549904e3ae68b6d4c4b12e1caab320a0dac17c6
HieuV <hieu_vuong75@yahoo.com>
2017-06-05 19:17:09 +1000

    finish customer

--- src/asgn2Customers/Customer.java
+++ src/asgn2Customers/Customer.java
@@ -3,95 +3,119 @@ package asgn2Customers;
 import asgn2Exceptions.CustomerException;
 
 /** An abstract class to represent a customer at the Pizza Palace restaurant.
- *  The Customer class is used as a base class of PickUpCustomer, 
+ *  The Customer class is used as a base class of PickUpCustomer,
  *  DriverDeliveryCustomer and DroneDeliverCustomer. Each of these subclasses overwrites
  *  the abstract method getDeliveryDistance. A description of the class's
- * fields and their constraints is provided in Section 5.2 of the Assignment Specification.  
- * 
- * @author Person B
+ * fields and their constraints is provided in Section 5.2 of the Assignment Specification.
+ *
+ * @author Hieu Vuong
 */
 public abstract class Customer {
-
+	private String name, mobileNumber, type;
+	private int locationX, locationY;
+	private final static int maxDistance = 10;
 
 	/**
 	 *  This class represents a customer of the Pizza Palace restaurant.  A detailed description of the class's fields
-	 *  and parameters is provided in the Assignment Specification, in particular in Section 5.2. 
+	 *  and parameters is provided in the Assignment Specification, in particular in Section 5.2.
 	 *  A CustomerException is thrown if the any of the constraints listed in Section 5.2 of the Assignment Specification
-	 *  are violated. 
-	 *  
+	 *  are violated.
+	 *
   	 * <P> PRE: True
   	 * <P> POST: All field values are set
-  	 * 
-	 * @param name - The Customer's name 
+  	 *
+	 * @param name - The Customer's name
 	 * @param mobileNumber - The customer mobile number
-	 * @param locationX - The customer x location relative to the Pizza Palace Restaurant measured in units of 'blocks' 
-	 * @param locationY - The customer y location relative to the Pizza Palace Restaurant measured in units of 'blocks' 
+	 * @param locationX - The customer x location relative to the Pizza Palace Restaurant measured in units of 'blocks'
+	 * @param locationY - The customer y location relative to the Pizza Palace Restaurant measured in units of 'blocks'
 	 * @param type - A human understandable description of this Customer type
-	 * @throws CustomerException if supplied parameters are invalid 
-	 * 
+	 * @throws CustomerException if supplied parameters are invalid
+	 *
 	 */
 	public Customer(String name, String mobileNumber, int locationX, int locationY, String type) throws CustomerException{
 		// TO DO
+		if (name.trim().length() <= 1) { //name exception
+			throw new CustomerException("Error: Invalid customer's name");
+		}
+		if (mobileNumber.trim().length() < 10 || mobileNumber.trim().length() > 10 ||
+				mobileNumber.trim().startsWith("0") == false) { //mobile number exception
+			throw new CustomerException("Error: Invalid mobile number");
+		}
+		if (Integer.toString(locationX).trim().length() == 0 || Math.abs(locationX) > maxDistance) { //coordinates expcetion
+			throw new CustomerException("Error: Invalid location coordinate(s)");
+		}
+		if (Integer.toString(locationY).trim().length() == 0 || Math.abs(locationY) > maxDistance) { //coordinates expcetion
+			throw new CustomerException("Error: Invalid location coordinate(s)");
+		}
+		if (type.trim().length() == 0) { //customer type exception
+			throw new CustomerException("Error: Invalid customer type");
+		}
+		this.name = name.trim(); this.mobileNumber = mobileNumber.trim();
+		this.locationX = locationX; this.locationY = locationY; this.type = type.trim();
 	}
-	
+
 	/**
 	 * Returns the Customer's name.
 	 * @return The Customer's name.
 	 */
 	public final String getName(){
 		// TO DO
+		return name;
 	}
-	
+
 	/**
 	 * Returns the Customer's mobile number.
 	 * @return The Customer's mobile number.
 	 */
 	public final String getMobileNumber(){
 		// TO DO
+		return mobileNumber;
 	}
 
 	/**
-	 * Returns a human understandable description of the Customer's type. 
-	 * The valid alternatives are listed in Section 5.2 of the Assignment Specification. 
+	 * Returns a human understandable description of the Customer's type.
+	 * The valid alternatives are listed in Section 5.2 of the Assignment Specification.
 	 * @return A human understandable description of the Customer's type.
 	 */
 	public final String getCustomerType(){
 		// TO DO
+		return type;
 	}
-	
+
 	/**
-	 * Returns the Customer's X location which is the number of blocks East or West 
-	 * that the Customer is located relative to the Pizza Palace restaurant. 
+	 * Returns the Customer's X location which is the number of blocks East or West
+	 * that the Customer is located relative to the Pizza Palace restaurant.
 	 * @return The Customer's X location
 	 */
 	public final int getLocationX(){
 		// TO DO
+		return locationX;
 	}
 
 	/**
-	 * Returns the Customer's Y location which is the number of blocks North or South 
-	 * that the Customer is located relative to the Pizza Palace restaurant. 
+	 * Returns the Customer's Y location which is the number of blocks North or South
+	 * that the Customer is located relative to the Pizza Palace restaurant.
 	 * @return The Customer's Y location
 	 */
 	public final int getLocationY(){
 		// TO DO
+		return locationY;
 	}
 
 	/**
-	 * An abstract method that returns the distance between the Customer and 
-	 * the restaurant depending on the mode of delivery. 
+	 * An abstract method that returns the distance between the Customer and
+	 * the restaurant depending on the mode of delivery.
 	 * @return The distance between the restaurant and the Customer depending on the mode of delivery.
 	 */
 	public abstract double getDeliveryDistance();
 
-	
-	
+
 	/**
-	 * Compares *this* Customer object with an instance of an *other* Customer object and returns true if  
+	 * Compares *this* Customer object with an instance of an *other* Customer object and returns true if
 	 * if the two objects are equivalent, that is, if the values exposed by public methods are equal.
 	 *  You do not need to test this method.
-	 * 
-	 * @return true if *this* Customer object and the *other* Customer object have the same values returned for 	
+	 *
+	 * @return true if *this* Customer object and the *other* Customer object have the same values returned for
 	 * getName(),getMobileNumber(),getLocationX(),getLocationY(),getCustomerType().
 	 */
 	@Override
@@ -99,10 +123,10 @@ public abstract class Customer {
 		Customer otherCustomer = (Customer) other;
 
 		return ( (this.getName().equals(otherCustomer.getName()))  &&
-			(this.getMobileNumber().equals(otherCustomer.getMobileNumber())) && 
-			(this.getLocationX() == otherCustomer.getLocationX()) && 
-			(this.getLocationY() == otherCustomer.getLocationY()) && 
-			(this.getCustomerType().equals(otherCustomer.getCustomerType())) );			
+			(this.getMobileNumber().equals(otherCustomer.getMobileNumber())) &&
+			(this.getLocationX() == otherCustomer.getLocationX()) &&
+			(this.getLocationY() == otherCustomer.getLocationY()) &&
+			(this.getCustomerType().equals(otherCustomer.getCustomerType())) );
 	}
 
 }
--- src/asgn2Customers/CustomerFactory.java
+++ src/asgn2Customers/CustomerFactory.java
@@ -3,31 +3,52 @@ package asgn2Customers;
 
 import asgn2Exceptions.CustomerException;
 
+//manually added
+import asgn2Customers.DriverDeliveryCustomer;
+import asgn2Customers.DroneDeliveryCustomer;
+import asgn2Customers.PickUpCustomer;
+
 /**
- * A class that instantiates the subclasses of asgn2Customers.Customer using the Factory Method pattern. 
+ * A class that instantiates the subclasses of asgn2Customers.Customer using the Factory Method pattern.
  * The classes are instantiated from one of the three valid customer codes outlined in
  * Section 5.3 of the Assignment Specification. Any other code will throw a CustomerException.   
- *     
+ *
  * @author Person B
  *
  */
 
 public class CustomerFactory {
+	private static String pickup = "PUC";
+	private static String drone = "DNC";
+	private static String driver = "DVC";
+	private static Customer a;
 
 	/**
-	 * A method that uses the Factory Method pattern to produce an instance of one of the asgn2Customers.Customer subclasses. 
+	 * A method that uses the Factory Method pattern to produce an instance of one of the asgn2Customers.Customer subclasses.
 	 * Subclasses are created using the customerCode. All valid customer codes are listed in Section 5.3 of the Assignment Specification.
-	 * A CustomerException should be thrown if an invalid customer code is used as a parameter. 
-	 * 
-	 * @param customerCode - A code indicating the subclass of asgn2Customers.Customer to instantiate. The valid codes are listed in Section 5.3 of the Assignment Specification. 
-	 * @param name - The customer's name 
+	 * A CustomerException should be thrown if an invalid customer code is used as a parameter.
+	 *
+	 * @param customerCode - A code indicating the subclass of asgn2Customers.Customer to instantiate. The valid codes are listed in Section 5.3 of the Assignment Specification.
+	 * @param name - The customer's name
 	 * @param mobileNumber - The customer mobile number
-	 * @param locationX - The customer x location relative to the Pizza Palace Restaurant measured in units of 'blocks' 
-	 * @param locationY  The customer y location relative to the Pizza Palace Restaurant measured in units of 'blocks' 
+	 * @param locationX - The customer x location relative to the Pizza Palace Restaurant measured in units of 'blocks'
+	 * @param locationY - The customer y location relative to the Pizza Palace Restaurant measured in units of 'blocks'
 	 * @return A valid PickUpCustomer, DriverDeliveryCustomer or DroneDeliveryCustomer depending on the customerCode.
-	 * @throws CustomerException if the customerCode is not one of the three valid codes listed in Section 5.3 of the Assignment Specification. 
+	 * @throws CustomerException if the customerCode is not one of the three valid codes listed in Section 5.3 of the Assignment Specification.
 	 */
 	public static Customer getCustomer(String customerCode, String name, String mobileNumber, int locationX,  int locationY) throws CustomerException{
 		// TO DO
+		if (customerCode.equals(pickup) == true) {
+			a = new PickUpCustomer(name, mobileNumber, locationX, locationY);
+		}
+		else if (customerCode.equals(drone) == true) {
+			a = new DroneDeliveryCustomer(name, mobileNumber, locationX, locationY);
+		}
+		else if (customerCode.equals(driver) == true) {
+			a = new DriverDeliveryCustomer(name, mobileNumber, locationX, locationY);
+		}
+		else throw new CustomerException("Error: Invalid customer code");
+
+		return a;
 	}
 }
